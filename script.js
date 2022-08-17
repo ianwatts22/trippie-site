42d11acbc31902a179e13bc03e898a1078ead7d8
ianwatts22 <ian@ianwatts.site>
2022-08-17 11:18:20 -0700

    removing interaction stuff, all Node.js stuff Co-authored-by: Alec Watts <alecwatts1@gmail.com>

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
 // threejs.org/docs/index.html#manual/en/introduction/Libraries-and-Plugins
 
 import * as THREE from 'three'
-//import * as CANNON from 'cannon-js'
 
 import {FontLoader} from 'https://unpkg.com/three@0.143.0/examples/jsm/loaders/FontLoader.js'
 import {ParametricGeometry} from 'https://unpkg.com/three@0.143.0/examples/jsm/geometries/ParametricGeometry.js'
 import {TextGeometry} from 'https://unpkg.com/three@0.143.0/examples/jsm/geometries/TextGeometry.js'
 import {OrbitControls} from 'https://unpkg.com/three@0.143.0/examples/jsm/controls/OrbitControls.js'
-// import * as CANNON from 'cannon-es'
-
-//import {Project, Scene3D, PhysicsLoader} from 'enable3d'
-
-
 
 // lil-gui: https://github.com/georgealways/lil-gui
 
-
 function main() {
-
   const canvas = document.querySelector('#c')
-  const renderer = new THREE.WebGLRenderer({canvas})
-  // document.body.appendChild( renderer.domElement )
+  const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
 
-  const fov = 40
-  const aspect = 2  // the canvas default
-  const near = 0.1
-  const far = 1000
+  let fov = 40
+  let aspect = 2  // the canvas default
+  let near = 0.1
+  let far = 1000
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  const cameraDist = 100
+  let cameraDist = 100
   camera.position.z = cameraDist
 
   //scene
   const scene = new THREE.Scene()
   scene.background = new THREE.Color(0x000000)
-  scene.add(new THREE.AxesHelper(5))
+  //scene.add(new THREE.AxesHelper(5))
 
-  //point light threejs.org/docs/#api/en/lights/PointLight
-  /* const light = new THREE.PointLight( 0xffffff, 1, 0 )
-  light.position.set( 0, 0, 0 )
-  scene.add(light) */
   const light2 = new THREE.PointLight( 0xffffff, 1, 100 )
   light2.position.set( 0, -100, 0 )
   scene.add(light2)
@@ -54,54 +41,47 @@ function main() {
   var box = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshNormalMaterial())
   box.geometry.translate(0, 0, 0.5)
   box.scale.set(1, 1, 3)
-  scene.add(box)
+  //scene.add(box)
 
-  
   // circles
-  let circles = [
+  let circlesDescriptions = [
   //["name",                 x,  y,  r]
-    ["eye",                  0,  0,  10],
-    ["Big 14 Music Video",   0,  0,  9],
-    ["BTS",                  0,  0,  8],
-    ["merch",                0,  0,  7],
-    ["tracklist",            0,  0,  6],
-    ["presave",              0,  0,  5],
-    ["random photos",        0,  0,  4],
-    ["tour life",            0,  0,  4],
-    ["fan letters",          0,  0,  3],
-    ["QR code",              0,  0,  3],
-    ["Twitch",               0,  0,  3],
-    ["fan art",              0,  0,  3]
+    ["assets/spinning.mp4",              0,  0,  6],
+    ["assets/clock.mp4",                  10,  5,  4],
+    ["assets/fanArt.mp4",                  3,  10,  3],
+    ["assets/eyes.mp4",                  -4,  15,  4],
+    ["assets/qrCode.png",                  -8,  7,  4],
   ]
-  const circleScale = 1
-  
+  let circleArr = []
+
   function addCircle([name, x, z, r]) {
-    let texture = new THREE.TextureLoader().load( name );
-   var qrMaterial = new THREE.MeshBasicMaterial({
-      map: qrTexture,
-      side: THREE.FrontSide,
-      toneMapped: false,
-    })
-    
-    let circleGeometry = new THREE.SphereGeometry(r, 32, 32, 0, Math.PI * 2, 0, 1)
-    let circleMaterial = new THREE.MeshBasicMaterial( { map: qrTexture, toneMapped: false,} )
-    let circle = new THREE.Mesh( circleGeometry, circleMaterial )
-    circle.scale.set(circleScale, circleScale, circleScale)
+    let texture
+    if (name.includes("mp4")) {
+      console.log("to")
+      //loading video texture *from HTML*
+      let video = document.getElementById(name)
+      video.play()
+      texture = new THREE.VideoTexture(video)
+    } else {
+      //loading image texture *from assets*
+      texture = new THREE.TextureLoader().load(name);
+    }
+    var material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.FrontSide, toneMapped: false, })
+    let geometry = new THREE.CircleGeometry (r, 32)
+    let circle = new THREE.Mesh( geometry, material )
+    circle.name = name
 
     circle.position.x = x
-    circle.position.y = 0
-    circle.position.z = z
+    circle.position.y = z
+    circle.position.z = 0
 
-    //scene.add(circle)
-    // circles.push(circle)
+    scene.add(circle)
+    circleArr.push(circle)
   }
 
-  // addCircle("test", 0, 0, 5)
-  
-  //making circles 
-  {
-    // circles.foreach(addCircle())
-  }
+  circlesDescriptions.forEach(element => {
+    addCircle(element)
+  });
   
 
   //–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -126,24 +106,42 @@ function main() {
   renderer.domElement.addEventListener('mousemove', onMouseMove, false)
   function onMouseMove(event) {
 
-    video.play()
-    spinVideo.play()
-
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
     raycaster.setFromCamera(mouse, camera)
     raycaster.ray.intersectPlane(plane, pointOfIntersection)
     box.lookAt(pointOfIntersection)
     var intersects = raycaster.intersectObjects(scene.children)//, true )
-    console.log(intersects[0].object.scale.x)
-    if (intersects[0].object) {
-      console.log("true")
+    if (intersects.length > 0) {
+      circleArr.forEach(element => {
+        element.scale.x = 1
+        element.scale.y = 1
+        element.scale.z = 1
+      });
+      //console.log("intersects object")
       intersects[0].object.scale.x = 1.1
       intersects[0].object.scale.y = 1.1
       intersects[0].object.scale.z = 1.1
+      console.log(intersects[0].object.name)
     }
-    intersects.scale.x = 2
   }
+
+  renderer.domElement.addEventListener("click", onclick, true);
+  var selectedObject;
+  function onclick(event) {
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children)//, true )
+    if (intersects.length > 0) {
+      //selectedObject = intersects[0];
+      console.log(intersects[0].object.name)
+      if (intersects[0].object.name === "assets/spinning.mp4") {
+        console.log("spinning")
+        window.open(
+          "https://www.geeksforgeeks.org", "_blank");
+      }
+    }
+  }
+  
   
   
   // orbit controls: threejs.org/docs/#examples/en/controls/OrbitControls
@@ -185,78 +183,12 @@ function main() {
   const planeBody = new CANNON.Body({ mass: 0 })
   planeBody.addShape(planeShape)
   planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
-  //orld.addBody(planeBody)
+  //world.addBody(planeBody)
 
-
-  let video = document.getElementById('video')
-  let videoTexture = new THREE.VideoTexture(video)
-
-  var movieMaterial = new THREE.MeshBasicMaterial({
-    map: videoTexture,
-    side: THREE.FrontSide,
-    toneMapped: false,
-  })
-
-  let spinVideo = document.getElementById('spinVideo')
-  let spinVideoTexture = new THREE.VideoTexture(spinVideo)
-
-  var spinMaterial = new THREE.MeshBasicMaterial({
-    map: spinVideoTexture,
-    side: THREE.FrontSide,
-    toneMapped: false,
-  })
-
-  let qrTexture = new THREE.TextureLoader().load( "assets/circleQR.png" );
-  var qrMaterial = new THREE.MeshBasicMaterial({
-      map: qrTexture,
-      side: THREE.FrontSide,
-      toneMapped: false,
-    })
-  //let movieGeometry = new THREE.BoxGeometry(100, 100, 100)
-  //let movieGeometry = new THREE.SphereGeometry(10, 32, 32, 0, Math.PI * 2, 0, 1.2)
-  let movieGeometry = new THREE.SphereGeometry(5, 32, 32, 0, Math.PI, 0, Math.PI)
-  let movieCubeScreen = new THREE.Mesh(movieGeometry, spinMaterial)
-  movieCubeScreen.position.set(0, 10, 0)
-  scene.add(movieCubeScreen)
-
-  let movieGeometry2 = new THREE.CircleGeometry( 3, 32 );
-  let movieCubeScreen2 = new THREE.Mesh(movieGeometry2, qrMaterial)
-  movieCubeScreen2.position.set(-9, 14, 0)
-  scene.add(movieCubeScreen2)
-
-  let movieGeometry3 = new THREE.CircleGeometry( 3, 32 );//THREE.SphereGeometry(2, 32, 32, 0, Math.PI, 0, Math.PI)
-  let movieCubeScreen3 = new THREE.Mesh(movieGeometry3, movieMaterial)
-  movieCubeScreen3.position.set(7, 3, 0)
-  scene.add(movieCubeScreen3)
-
-  
-  //sphere // eyeball    threejs.org/docs/?q=materi#api/en/geometries/SphereGeometry
-  var sphereGeometry = new THREE.SphereGeometry(10, 32, 32, 0, Math.PI * 2, 0, 1.2)
-  var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff, side: THREE.DoubleSide} )
-  var sphereMesh = new THREE.Mesh( sphereGeometry, sphereMaterial )
-  sphereMesh.geometry.translate(0,0,0)
-  sphereMesh.geometry.rotateZ(Math.PI)
-  sphereMesh.scale.set(1, 1, 1)
-  //scene.add(sphereMesh)
-
-  // const sphereShape = new CANNON.Sphere(30)
-  // const sphereBody = new CANNON.Body({ mass: 0 })
-  // sphereBody.addShape(sphereShape)
-  // sphereBody.position.x = sphereMesh.position.x
-  // sphereBody.position.y = sphereMesh.position.y
-  // sphereBody.position.z = sphereMesh.position.z
-  // world.addBody(sphereBody)
-
-  //   renderer.domElement.addEventListener("keydown", function (e) {
-  //     console.log("hello")
-  //     if (e.code === "Enter") {
-  //         console.log("hello")//checks whether the pressed key is "Enter"
-  //         validate(e)
-  //     }
-  // })
 
   const clock = new THREE.Clock()
   let delta
+
   function animate() {
 
     if (resizeRendererToDisplaySize(renderer)) {
@@ -276,12 +208,13 @@ function main() {
         cubeBody.position.y,
         cubeBody.position.z
     )
-    movieCubeScreen.quaternion.copy(camera.quaternion)
-    movieCubeScreen2.quaternion.copy(camera.quaternion)
-    movieCubeScreen3.quaternion.copy(camera.quaternion)
 
-    videoTexture.needsUpdate = true
-    spinVideoTexture.needsUpdate = true
+    circleArr.forEach(element => {
+      element.quaternion.copy(camera.quaternion)
+    });
+    
+    //videoTexture.needsUpdate = true
+    //spinVideoTexture.needsUpdate = true
     render()
       
     //const elapsedTime = clock.getElapsedTime()
@@ -289,12 +222,9 @@ function main() {
     //particlesMesh.rotation.y = mouseX * (elapsedTime *0.000008)
   }
   
-  function render() {
-      renderer.render(scene, camera)
-  }
+  function render() { renderer.render(scene, camera) }
   
   animate()
-
   requestAnimationFrame(render) // needed?
 }
 main()
