5f80dec998992d9e547ab96d45c18d039f8689ce
ianwatts22 <ian@ianwatts.site>
2022-08-17 18:38:00 -0700

    Co-authored-by: Alec Watts <alecwatts1@gmail.com>

--- camera.js
+++ camera.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { TWEEN } from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/18.6.4/tween.umd.js'
+// import { TWEEN } from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/18.6.4/tween.umd.js'
 
 export function createCamera() {
   let fov = 40
@@ -7,18 +7,22 @@ export function createCamera() {
   let near = 0.1
   let far = 1000
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  let cameraDist = 100
-  camera.position.set(0, 0, cameraDist)
+  camera.position.set(0, 0, 200)
   return camera
 }
 
 export function cameraMovements(camera) {
-  let cameraDist = 100
-  camera.position.z = cameraDist
+  const thisCamera = camera
   
-  var cameraTween = new TWEEN.Tween(camera.position).to({
-    x: 1,
-    y: 1,
-    z: 1
-  }, 5000)
+  thisCamera.position.set(0, 0, 100)
+  
+  const time = 5000
+  var cameraTween = new TWEEN.Tween(thisCamera.position).to({x: 1, y: 1, z: 40}, time)
+  var cameraTween2 = new TWEEN.Tween(thisCamera.rotation).to({x: 0, y: 0, z: Math.PI * 2}, time)
+  // .onComplete(() => {
+  //   object.userData.isTweening = false;
+  // });
+  cameraTween.start();
+  cameraTween2.start();
+  
 }
--- script.js
+++ script.js
@@ -2,38 +2,45 @@
 
 import * as THREE from 'three'
 
-import {FontLoader} from 'https://unpkg.com/three@0.143.0/examples/jsm/loaders/FontLoader.js'
-import {ParametricGeometry} from 'https://unpkg.com/three@0.143.0/examples/jsm/geometries/ParametricGeometry.js'
-import {TextGeometry} from 'https://unpkg.com/three@0.143.0/examples/jsm/geometries/TextGeometry.js'
-import {OrbitControls} from 'https://unpkg.com/three@0.143.0/examples/jsm/controls/OrbitControls.js'
-//import { TWEEN } from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/18.6.4/tween.umd.js'
-import {createCamera} from "./camera.js"
+import { FontLoader } from 'https://unpkg.com/three@0.143.0/examples/jsm/loaders/FontLoader.js'
+import { ParametricGeometry } from 'https://unpkg.com/three@0.143.0/examples/jsm/geometries/ParametricGeometry.js'
+import { TextGeometry } from 'https://unpkg.com/three@0.143.0/examples/jsm/geometries/TextGeometry.js'
+import { OrbitControls } from 'https://unpkg.com/three@0.143.0/examples/jsm/controls/OrbitControls.js'
+// import { TWEEN } from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/18.6.4/tween.umd.js'
+import { createCamera, cameraMovements } from "./camera.js"
 //import {animate} from "./animate.js"
-import "./three.interaction.js"
+
+// INTERACTION
+// import "./interaction/three.interaction.js"
+// import { Interaction } from './interaction/build/three.interaction.js';
+// import { Interaction } from 'https://unpkg.com/three.interaction@0.2.3/build/three.interaction.js'
 
 
 // lil-gui: https://github.com/georgealways/lil-gui
 
 function main() {
   const canvas = document.querySelector('#c')
-  const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
-
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
   const camera = createCamera()
 
-  var cameraTween = new TWEEN.Tween(camera.position).to({
-    x: 1,
-    y: 1,
-    z: 1
-  }, 5000)
-  // .onComplete(() => {
-  //   object.userData.isTweening = false;
-  // });
-  //cameraTween.start();
+  // cameraMovements(camera)
+  /* const entryTime = 2000
+  var cameraTween = new TWEEN.Tween(camera.position).to({ x: 1, y: 1, z: 40 }, entryTime)
+  var cameraTween2 = new TWEEN.Tween(camera.rotation).to({ x: 0, y: 0, z: Math.PI * 2 }, entryTime)
+  cameraTween.start();
+  cameraTween2.start(); */
+
+
 
   //scene
   const scene = new THREE.Scene()
-  scene.background = new THREE.Color(0x000000)
+  let backgroundTexture = new THREE.TextureLoader().load("assets/background.png") //loading image texture *from assets*
+  //scene.background = new THREE.Color(0x000000)
+  scene.background = backgroundTexture
+
   //scene.add(new THREE.AxesHelper(5))
+
+  // const interaction = new Interaction(renderer, scene, camera);
 
 
   // pointer box
@@ -42,23 +49,26 @@ function main() {
   box.scale.set(1, 1, 3)
   //scene.add(box)
 
+
   //eyeball
   let eyeTexture = new THREE.TextureLoader().load("assets/eyeball.png")
-
   var eyeMaterial = new THREE.MeshBasicMaterial({ map: eyeTexture, side: THREE.FrontSide, toneMapped: false, })
   var eyeGeometry = new THREE.SphereBufferGeometry(20, 32, 32)
   var eyeball = new THREE.Mesh(eyeGeometry, eyeMaterial)
   eyeball.position.set(0, 0, 30)
+  eyeball.name = "eyeball"
+  eyeball.scale.set(1.2, 1.2, 1.2)
   scene.add(eyeball)
+
 
   // circles
   let circlesDescriptions = [
-  //["name",                 x,  y,  r, link]
-    ["assets/spinning.mp4",  0 , 0 , 6, "https://www.youtube.com/watch?v=_q-_q-_q-_q"],
-    ["assets/clock.mp4"   ,  10, 5 , 4, "none"                                       ],
-    ["assets/fanArt.mp4"  ,  3 , 10, 3, "none"                                       ],
-    ["assets/eyes.mp4"    , -4 , 15, 4, "none"                                       ],
-    ["assets/qrCode.png"  , -8 , 7 , 4, "none"                                       ],
+    //["name",                 x,  y,  r, link]
+    ["assets/spinning.mp4", 0, 0, 6, "https://www.youtube.com/watch?v=_q-_q-_q-_q"],
+    ["assets/clock.mp4", 10, 5, 4, "none"],
+    ["assets/fanArt.mp4", 3, 10, 3, "none"],
+    ["assets/eyes.mp4", -4, 15, 4, "none"],
+    ["assets/qrCode.png", -8, 7, 4, "none"],
   ]
   let circleArr = []
 
@@ -72,12 +82,12 @@ function main() {
       texture = new THREE.TextureLoader().load(name) //loading image texture *from assets*
     }
     var material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.FrontSide, toneMapped: false, })
-    let geometry = new THREE.CircleGeometry (r, 32)
-    let circle = new THREE.Mesh( geometry, material )
-    
+    let geometry = new THREE.CircleGeometry(r, 32)
+    let circle = new THREE.Mesh(geometry, material)
+
     circle.name = name
     circle.userData.isTweening = false
-    circle.position.set(x, y, 0)
+    circle.position.set(x, y - 6, 0)
 
     scene.add(circle)
     circleArr.push(circle)
@@ -92,63 +102,70 @@ function main() {
   var pointOfIntersection = new THREE.Vector3()
   var mouse = new THREE.Vector2()
 
+  let hoveredObject
+  let hoveringObject = false
+
   renderer.domElement.addEventListener('mousemove', onMouseMove, false)
   function onMouseMove(event) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
     eyeball.lookAt(pointOfIntersection)
-    
+
     raycaster.setFromCamera(mouse, camera)
     raycaster.ray.intersectPlane(plane, pointOfIntersection)
 
     var intersects = raycaster.intersectObjects(scene.children)//, true )
-    // if (intersects.length > 0) {
-    //   let object = intersects[0].object
-    //   if (object.userData.isTweening) return;
-    //   var tweenInflate = new TWEEN.Tween(object.scale).to({
-    //     x: 1.2,
-    //     y: 1.2,
-    //     z: 1.2
-    //   }, 1500).easing(TWEEN.Easing.Elastic.Out).onStart(() => {
-    //     object.userData.isTweening = true;
-    //   });
-    //   var tweenDeflate = new TWEEN.Tween(object.scale).to({
-    //     x: 1,
-    //     y: 1,
-    //     z: 1
-    //   }, 1000).onComplete(() => {
-    //     object.userData.isTweening = false;
-    //   });
-    //   tweenInflate.chain(tweenDeflate);
-    //   tweenInflate.start();
+    if (intersects.length > 0) {
+      let object = intersects[0].object
+      hoveringObject = true
+      hoveredObject = intersects[0].object
+      if (object.userData.isTweening) return
+      var tweenInflate = new TWEEN.Tween(object.scale)
+        .to({ x: 1.2, y: 1.2, z: 1.2 }, 250)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onStart(() => { object.userData.isTweening = true; })
+        .onComplete(() => { object.userData.isTweening = false; })
+      tweenInflate.start();
+    } else {
+      hoveringObject = false
+    }
     // } else {
     //   circleArr.forEach(element => {
-    //     var targetPosition = new THREE.Vector3(1, 1, 1);
-    //     new TWEEN.Tween( element.scale )
-    //     .to( targetPosition )
-    //     .easing( TWEEN.Easing.Cubic.InOut )
-    //     .start();
-
-    //     //element.scale.set(1, 1, 1)
+    //     //if (element.userData.isTweening) return
+    //     var tweenDeflate = new TWEEN.Tween(element.scale)
+    //       .to({ x: 1, y: 1, z: 1 }, 500)
+    //       .easing(TWEEN.Easing.Cubic.InOut)
+    //       //.onStart(() => { element.userData.isTweening = true; })
+    //       //.onComplete(() => { element.userData.isTweening = false; })
+    //       //.start();
+    //     tweenDeflate.start()
     //   });
     // }
   }
 
+
   renderer.domElement.addEventListener("click", onclick, true);
   function onclick(event) {
-    raycaster.setFromCamera(mouse, camera);
+    raycaster.setFromCamera(mouse, camera)
     var intersects = raycaster.intersectObjects(scene.children)//, true )
     if (intersects.length > 0) {
+      console.log("clicked on")
       circlesDescriptions.forEach(element => {
-        if (element[4] != "none"  && intersects[0].object.name === element[0]) {
+        if (element[4] != "none" && intersects[0].object.name === element[0]) {
           window.open(element[4], "_blank") //open link in new tab
         }
       });
+      if (intersects[0].object.name === "eyeball") {
+        console.log("eyeball")
+        const entryTime = 2000
+        new TWEEN.Tween(camera.position).to({ x: 1, y: 1, z: 40 }, entryTime).start()
+        new TWEEN.Tween(camera.rotation).to({ x: 0, y: 0, z: Math.PI * 2 }, entryTime).start()
+      }
     }
   }
-  
+
   // orbit controls: threejs.org/docs/#examples/en/controls/OrbitControls
-  const controls = new OrbitControls( camera, renderer.domElement )
+  const controls = new OrbitControls(camera, renderer.domElement)
   controls.enableDamping = true
   controls.update()
 
@@ -167,29 +184,44 @@ function main() {
   const clock = new THREE.Clock()
   var delta = Math.min(clock.getDelta(), 0.1)
 
-  function animate() {  
+  function animate() {
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement
       camera.aspect = canvas.clientWidth / canvas.clientHeight
       camera.updateProjectionMatrix()
     }
     requestAnimationFrame(animate)
-  
-  
+
+
     circleArr.forEach(element => {
       element.quaternion.copy(camera.quaternion)
     });
+
+    circleArr.forEach(element => {
+      if (element.userData.isTweening) return
+      if (hoveringObject) {
+        if (element == hoveredObject) return
+      }
+      var tweenDeflate = new TWEEN.Tween(element.scale)
+        .to({ x: 1, y: 1, z: 1 }, 250)
+        .easing(TWEEN.Easing.Cubic.InOut)
+        .onStart(() => { element.userData.isTweening = true; })
+        .onComplete(() => { element.userData.isTweening = false; })
+        //.start();
+      tweenDeflate.start()
+    });
+
     //videoTexture.needsUpdate = true
     TWEEN.update()
     render()
-      
+
     //const elapsedTime = clock.getElapsedTime()
     //particlesMesh.rotation.x = -mouseY * (elapsedTime *0.000008)
     //particlesMesh.rotation.y = mouseX * (elapsedTime *0.000008)
   }
 
   function render() { renderer.render(scene, camera) }
-  
+
   animate()
   requestAnimationFrame(render) // needed?
 
